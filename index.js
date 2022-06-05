75993adb8944f3b976f4b3f4ceb4729974f1dcb8
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-06-05 11:23:39 +0530

    Rebranding and API field changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const api_url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9a12834df62bc3ebee87c993a0e4c95e"; 
+// const api_url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9a12834df62bc3ebee87c993a0e4c95e"; 
+const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=28.676422&lon=77.115112&appid=9a12834df62bc3ebee87c993a0e4c95e&units=metric"; 
 
 fetch(api_url)
   .then(response => response.json())
