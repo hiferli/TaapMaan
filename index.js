0d9da73554ef07439838e2f4713d75d68d7bf4d8
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-06-10 13:52:53 +0530

    jQuery to merge Country Dropdown HTML file

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-// const api_url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=9a12834df62bc3ebee87c993a0e4c95e"; 
+// const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=28.676422&lon=77.115112&appid=9a12834df62bc3ebee87c993a0e4c95e&units=metric"; 
 const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=28.676422&lon=77.115112&appid=9a12834df62bc3ebee87c993a0e4c95e&units=metric"; 
 
 // console.log(fetch(api_url))
 
 fetch(api_url)
   .then(response => response.json())
-  .then(data =>
-    {let main_object = data.main
-     let cordinates = data.coord
-     document.getElementById('latitude').innerText += cordinates['lat']
-     document.getElementById('longitude').innerText += cordinates['lon']
-    document.getElementById('temperature').innerText += main_object['temp'];
+  .then(
+    data =>
+      {
+        console.log(data)
+        // Making required objects
+        const main_object = data.main
+        const cordinates_object = data.coord
 
-    // for (const property in main_object) {
-      // console.log(`${property}: ${main_object[property]}`);
-    // }
-  }
+        document.getElementById('latitude').innerText += cordinates_object['lat']
+        document.getElementById('longitude').innerText += cordinates_object['lon']
+        document.getElementById('temperature').innerText += main_object['temp'];
+        console.log(data.weather[0]['description'])
+        console.log(data.sys['country'])
+    }
   )
   .catch(error => console.log('Error: ' + error))
 
