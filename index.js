e90e50d7c3c4944a5bfd72688a168cb2365b9e75
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-06-11 14:43:21 +0530

    OnChange function to get selected country

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=28.676422&lon=77.115112&appid=9a12834df62bc3ebee87c993a0e4c95e&units=metric"; 
 const api_url = "https://api.openweathermap.org/data/2.5/weather?lat=28.676422&lon=77.115112&appid=9a12834df62bc3ebee87c993a0e4c95e&units=metric"; 
 
-// console.log(fetch(api_url))
+function countryChange(){
+  console.log($('#country').find('option:selected').val())
+}
 
 fetch(api_url)
   .then(response => response.json())
@@ -16,8 +18,8 @@ fetch(api_url)
         document.getElementById('latitude').innerText += cordinates_object['lat']
         document.getElementById('longitude').innerText += cordinates_object['lon']
         document.getElementById('temperature').innerText += main_object['temp'];
-        console.log(data.weather[0]['description'])
-        console.log(data.sys['country'])
+        // console.log(data.weather[0]['description'])
+        // console.log(data.sys['country'])
     }
   )
   .catch(error => console.log('Error: ' + error))
